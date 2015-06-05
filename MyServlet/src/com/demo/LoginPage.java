7163cf29278d519d003b606f94460b6c54723c49
dewendra <dewendra1.singh@gmail.com>
2015-06-05 17:09:07 +0530

    My servlet first demo

---
+++ MyServlet/src/com/demo/LoginPage.java
@@ -0,0 +1,55 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginPage
+ */
+@WebServlet("/LoginPage")
+public class LoginPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter s=response.getWriter();
+		s.write("<html");
+		s.write("<Body");
+		s.write("<title>Register Page</title>");
+		s.write("<b>This is my first Servlet Page On Servlet 3.0");
+
+		s.write("</title>");
+
+		s.write("<Body");
+
+		s.write("</html");
+		s.flush();
+		s.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
